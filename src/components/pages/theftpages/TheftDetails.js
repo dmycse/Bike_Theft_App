eb5460a84ffba027cd6089a03f82b6baf50dd680
dmycse <dmycse@gmail.com>
2023-04-16 12:39:26 +0300

    Update TheftDetails component

--- src/components/pages/theftpages/TheftDetails.js
+++ src/components/pages/theftpages/TheftDetails.js
@@ -1,137 +1,357 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-
+import React, { useState} from 'react';
+import { Link, useParams, useNavigate, Navigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchTheftRecords, fetchTheftRemove, status, bikeType, bikeColor } from 'components/store/theftSlice';
+import { fetchChangeTheftCase, status, bikeType, bikeColor } from 'components/store/theftSlice';
 
 import { Formik, Form } from 'formik';
 import FormControl from 'components/pages/forms/FormControl';
+import { theftRecordSchema } from 'components/pages/forms/schema';
 
 import Button from 'components/pages/Button';
+import ModalWindow from 'components/pages/ModalWindow';
 
 import css from 'components/pages/theftpages/TheftStyles.module.scss';
 
 
-let handleEnterPress = (e) => {
-	if (e.key  === 'Enter') {  
-		e.preventDefault();
-		return false;
-	}
-};
-
-
-function TheftsRecords() {
+function TheftDetails() {
+	
 	let navigate = useNavigate();
-	let location = useLocation();
+	let {_id} = useParams();
 
+	let [modalWindowIsOpen, setModalWindowIsOpen] = useState(false);
+	
 	let dispatch = useDispatch();
-	let { theftCase, fetchStatus, fetchError } = useSelector(state => state.theftRecords);
-	let employees = useSelector(state => state.employeeList.employees);
-	let approvedEmployee = employees.filter(employee => employee.approved);
+	let {theftCase, fetchStatus, fetchError} = useSelector(state => state.theftRecords);
+	let savedValues = theftCase.find(item => item._id === _id);
 
-	let activeTheftCases = theftCase.filter(item => item.status !== 'done')
+	let approvedEmployee = useSelector(state => state.employeeList.employees).filter(employee => employee.approved);
+	
+	let officerList = [{key: 'Выберите из списка', value: ''}, ...approvedEmployee.map(employee => (
+		{key: `${employee.lastName} ${employee.firstName}`, value: employee._id}))];
 
-	let [statusFilter, setStatusFilter] = useState('');
+	if(!savedValues) {
+		return <Navigate to='/*' />
+	}
 
-	let printFullName = (id) => {
-		let officer = employees.find(employee => employee._id === id);
-		let officerFullName = `${officer.lastName} ${officer.firstName}`;
-		return officerFullName;
-	};
+	let toggleModal= () => {
+    setModalWindowIsOpen(!modalWindowIsOpen);
+		navigate('/theftrecords');
+  };
 
-	let printFieldName = (value, name) => {
-		let chossedKey =  name.find(item => item.value === value);
-		return chossedKey.key;
-	};
-
-	let handleRemove = (e, id) => {
-		e.stopPropagation();
-			dispatch(fetchTheftRemove(id));
-	};
-
-	let goTo = () => navigate('/', {state: location});
-
-	useEffect(() => {
-		dispatch(fetchTheftRecords())
-	}, [dispatch]);
-
+	let onSubmit = (values, {setSubmitting}) => {
+		try {
+			dispatch(fetchChangeTheftCase(values));
+			setSubmitting(false);
+			setModalWindowIsOpen(!modalWindowIsOpen);
+		}
+		catch(err) {
+			console.error(err.message)
+		}
+	}
+  
 	return (
-		<article className={css.theftrecords}>
+		<div className={css.theftdetails}>
 			<div className={css.theftrecords__title_container}>
-				<h1 className={css.theftrecords__title}>Сообщения о кражах</h1>
-				<div className={css.theftrecords__info}>
-					Активных сообщений о кражах: <span className={css.theftrecords__info_bold}>{activeTheftCases.length}</span>
+				<h1 className={`${css.theftrecords__title} ${css.theftdetails__title}`}>Сообщение о краже {_id}</h1>
+				<div className={`${css.theftrecords__info} ${css.theftdetails__info}`}>
+					ClientID: <span>{savedValues.clientId}</span>
 				</div>
 			</div>
-			<Button 
-				type='button'
-				label='Coздать сообщение о краже'
-				className={css.theftrecords__btn}
-				onClick={goTo}
-			/>
-			{fetchError && <h3 className={css.theftrecords__msgErr}>Ошибка загрузки данных: {fetchError} !</h3>}
-			{fetchStatus === 'loading' ? <h3 className={css.theftrecords__msg}>Загрузка данных...</h3> : (theftCase.length < 1 ? <h3 className={css.theftrecords__msg}>Сообщений о кражах нет</h3> : 
-			<> 
-				<Formik>
-					{() => (
-						<Form className={css.theftrecords__search}>
-							<FormControl 
-								control='select' 
-								name='status' 
-								options={status} 
-								label='Найти по статусу: ' 
-								onChange={e => setStatusFilter(e.target.value)}
-								onKeyPress={handleEnterPress}
-								errors={''}
-								touched={''} 
-							/>
-						</Form>
-						)	
-					}
-				</Formik>
-				<section className='table__container'>
-					<table className='table'>
-						<thead className='table__head'>
-							<tr>
-								<th className='table__th table__th_mobile'>Статус</th>
-								<th className='table__th'>Лицензия</th>
-								<th className='table__th table__th_tablet'>Арендатор</th>
-								<th className='table__th table__th_mobile'>Тип</th>
-								<th className='table__th table__th_tablet'>Цвет</th>
-								<th className='table__th'>Дата кражи</th>
-								<th className='table__th table__th_tablet'>Сотрудник</th>
-								<th className='table__th'>Удалить</th>
-							</tr>
-						</thead>
-						<tbody className='table__body'>
-							{theftCase
-								.filter(elem => (statusFilter === '') ? elem : elem.status === statusFilter)
-								.map(item => (
-									<tr key={item._id} onClick={() => navigate(`/theftrecords/${item._id}`)} className={`table__body_tr ${item.status === 'done' && 'table__body_done'}`}>
-										<td className='table__td table__td_txt table__td_mobile'>{item.status ? printFieldName(item.status, status) : 'не определён'}</td>
-										<td className='table__td table__td_txt'>{item.licenseNumber}</td>
-										<td className='table__td table__td_tablet'>{item.ownerFullName}</td>
-										<td className='table__td table__td_txt table__td_mobile'>{item.type ? printFieldName(item.type, bikeType) : 'не выбран'}</td>
-										<td className='table__td table__td_txt table__td_tablet'>{item.color ? printFieldName(item.color, bikeColor) : 'нет данных'}</td>
-										<td className='table__td table__td_txt'>{new Date(item.date).toLocaleDateString()}</td>
-										<td className='table__td table__td_txt table__td_tablet'>{approvedEmployee.find(elem => elem._id === item.officer) ? printFullName(item.officer) : 'не назначен'}</td>
-										<td className='table__td table__td_btn'>
-											<Button 
-												type='button'
-												label='Удалить'
-												className='table__btn'
-												onClick={(e) => handleRemove(e, item._id)}
-											/>
-										</td>
-									</tr>
-								))}
+			{fetchStatus === 'loading' && <h3 className={css.theftdetails__msg}>Загрузка данных...</h3>}
+			{fetchError && <h3 className={css.theftdetails__msgErr}>Ошибка загрузки данных: {fetchError} !</h3>}
+			<Formik
+				initialValues = {{...savedValues, newTheftDate: null}}
+				validationSchema = {theftRecordSchema}
+				onSubmit = {onSubmit}
+			>
+			{({values, isSubmitting, dirty, errors, touched}) => {
+				return (
+					<Form>
+						<table className={css.theftdetails__table_desktop}>
+							<tbody>
+								<tr>
+									<td className={css.theftdetails__detail}>Cтатус:</td>
+									<td className={css.theftdetails__secondtd}>
+										<FormControl 
+											control='select' 
+											name='status' 
+											options={status}
+											errors={errors}
+											touched={touched}  
+										/>
+									</td>
+									<td className={css.theftdetails__tdspacer}></td>
+									<td className={css.theftdetails__detail}>Дата кражи:</td>
+									<td>
+										<FormControl 
+											control='date' 
+											name='newTheftDate' 
+											maxDate={new Date()}
+											disabled = {values.status === 'done'}
+											placeholderText={new Date(savedValues.date).toLocaleDateString()}
+											errors={''}
+											touched={''} 
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Номер лицензии: </td>
+									<td>
+										<FormControl
+											control='input'
+											name='licenseNumber'
+											disabled = {values.status === 'done'}
+											errors={errors}
+											touched={touched} 
+										/>
+									</td>
+									<td className={css.theftdetails__tdspacer}></td>
+									<td className={css.theftdetails__detail}>Дата создания: </td>
+									<td className={css.theftdetails__detail_data}>
+										{new Date(savedValues.createdAt).toLocaleDateString()}
+									</td>
+								</tr>
+								<tr>
+								<td className={css.theftdetails__detail}>ФИО арендатора: </td>
+									<td>
+										<FormControl 
+											control='input' 
+											name='ownerFullName'
+											errors={errors}
+											touched={touched} 
+											disabled = {values.status === 'done'} 
+										/>
+									</td>
+									<td className={css.theftdetails__tdspacer}></td>
+									<td className={css.theftdetails__detail}>Дата обновления: </td>
+									<td className={css.theftdetails__detail_data}>
+										{new Date(savedValues.updatedAt).toLocaleDateString()}
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Тип велосипеда: </td>
+									<td>
+										<FormControl 
+											control='select' 
+											name='type' 
+											options={bikeType}
+											errors={errors}
+											touched={touched} 
+											disabled = {values.status === 'done'}
+										/>
+									</td>
+									<td className={css.theftdetails__tdspacer}></td>
+									<td className={css.theftdetails__detail}>Цвет велосипеда: </td>
+									<td>
+										<FormControl
+											control='select'
+											name='color'
+											options={bikeColor}
+											errors={''}
+											touched={''} 
+											disabled = {values.status === 'done'} 
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Дополнительная информация: </td>
+									<td colSpan="4">
+										<FormControl 
+											control='textarea' 
+											name='description' 
+											errors={''}
+											touched={''}
+											disabled = {values.status === 'done'} 
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Ответственный: </td>
+									<td>
+										<FormControl 
+											control='select' 
+											type='select' 
+											name='officer' 
+											options={officerList}
+											disabled = {values.status === 'done'}
+											errors={''}
+											touched={''} 
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Завершающий комментарий: </td>
+									<td colSpan="4">
+										<FormControl 
+											control='textarea' 
+											name='resolution'
+											errors={errors}
+											touched={touched}
+											disabled = {values.status !== 'done'}
+										/>
+									</td>
+								</tr>
 							</tbody>
-					</table>
-				</section>
-			</>
-			)}
-		</article>
+						</table>
+						<table className={css.theftdetails__table_mobile}>
+							<tbody>
+								<tr>
+									<td className={css.theftdetails__detail}>Cтатус:</td>
+									<td className={css.theftdetails__secondtd}>
+										<FormControl 
+											control='select'
+											id='status2'
+											name='status' 
+											options={status}
+											errors={errors}
+											touched={touched}  
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Дата кражи:</td>
+									<td>
+										<FormControl 
+											control='date'
+											id='newTheftDate2'
+											name='newTheftDate' 
+											maxDate={new Date()}
+											disabled = {values.status === 'done'}
+											placeholderText={new Date(savedValues.date).toLocaleDateString()}
+											errors={''}
+											touched={''} 
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Номер лицензии: </td>
+									<td>
+										<FormControl
+											control='input'
+											id='licenseNumber2'
+											name='licenseNumber'
+											disabled = {values.status === 'done'}
+											errors={errors}
+											touched={touched} 
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Дата создания: </td>
+									<td className={css.theftdetails__detail_data}>
+										{new Date(savedValues.createdAt).toLocaleDateString()}
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>ФИО арендатора: </td>
+									<td>
+										<FormControl 
+											control='input'
+											id='ownerFullName2'
+											name='ownerFullName'
+											errors={errors}
+											touched={touched} 
+											disabled = {values.status === 'done'} 
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Дата обновления: </td>
+									<td className={css.theftdetails__detail_data}>
+										{new Date(savedValues.updatedAt).toLocaleDateString()}
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Тип велосипеда: </td>
+									<td>
+										<FormControl 
+											control='select'
+											id='type2' 
+											name='type' 
+											options={bikeType}
+											errors={errors}
+											touched={touched} 
+											disabled = {values.status === 'done'}
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Цвет велосипеда: </td>
+									<td>
+										<FormControl
+											control='select'
+											id='color2'
+											name='color'
+											options={bikeColor}
+											errors={''}
+											touched={''} 
+											disabled = {values.status === 'done'} 
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Дополнительная информация: </td>
+									<td>
+										<FormControl 
+											control='textarea'
+											id='description2' 
+											name='description' 
+											errors={''}
+											touched={''}
+											disabled = {values.status === 'done'} 
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Ответственный: </td>
+									<td>
+										<FormControl 
+											control='select' 
+											type='select'
+											id='officer2'  
+											name='officer' 
+											options={officerList}
+											disabled = {values.status === 'done'}
+											errors={''}
+											touched={''} 
+										/>
+									</td>
+								</tr>
+								<tr>
+									<td className={css.theftdetails__detail}>Завершающий комментарий: </td>
+									<td>
+										<FormControl 
+											control='textarea'
+											id='resolution2' 
+											name='resolution'
+											errors={errors}
+											touched={touched}
+											disabled = {values.status !== 'done'}
+										/>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<Button
+							label='Cохранить изменения' 
+							className={`form__btn ${css.theftdetails__btn}`}
+							disabled={isSubmitting || !dirty}
+						/>
+						<Link to='/theftrecords' className={`form__btn ${css.theftdetails__btn}`}>Закрыть</Link>
+					</Form>
+					)
+				}
+			}
+			</Formik>
+			<ModalWindow
+				isOpen={modalWindowIsOpen}
+				onRequestClose={toggleModal}
+				title = {fetchStatus === 'loading' ? 'Обновление данных...' : fetchStatus === 'resolved' ? 'Данные обновлены' : ''}
+				textErr =  {fetchError ? `Ошибка: ${fetchError} !` : ''}
+				onClick = {toggleModal}
+			/>
+		</div>		
 	)
 }
 
-export default TheftsRecords;
+export default TheftDetails;
