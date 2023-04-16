9a00e63e3afc900fe29705eeae9f68ac6478bb71
dmycse <dmycse@gmail.com>
2023-04-16 11:42:42 +0300

    Correct paths in index.js of store

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import employeeSlice from './employeeSlice';
-import theftSlice from './theftSlice';
-import authSlice from './authSlice';
+import employeeSlice from 'components/store/employeeSlice';
+import theftSlice from 'components/store/theftSlice';
+import authSlice from 'components/store/authSlice';
 
 
 const store = configureStore({
